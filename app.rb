d22e30190797ed2e30925549294f3eadcd66fd8a
Tyler Copple <j.tylercopple@gmail.com>
2013-01-12 13:16:10 -0500

    Getting to a working baseline.

--- app.rb
+++ app.rb
@@ -4,10 +4,7 @@ require 'thin'
 require 'sass'
 require 'haml'
 require 'neat'
-
-require_relative 'routes/coffee'
-require_relative 'routes/sass'
-require_relative 'routes/view'
+require 'sinatra/assetpack'
 
 before do
   content_type :html, 'charset' => 'utf-8'
@@ -18,12 +15,31 @@ helpers do
   alias_method :h, :escape_html
 end
 
-class Wedding < Sinatra::Base
+class  AppHandler < Sinatra::Base
   set :root, File.dirname(__FILE__)
   set :haml, { :format => :html5 }
-  set :public_folder, root + '/public'
+  set :public_folder, "#{root}/public"
+  set :scss, { load_paths: ["#{AppHandler.root}/assets/stylesheets"] }
 
-  use SassHandler
-  use CoffeeHandler
-  use ViewHandler
+  register Sinatra::AssetPack
+  assets {
+    serve '/assets/js', from: 'assets/javascripts'
+    serve '/assets/css', from: 'assets/stylesheets'
+    serve '/assets/images', from: 'public/images'
+
+    css :app, ["/assets/css/app.css"]
+  }
+
+  get "/content/*" do
+    filename = "#{@root}/partials/_#{params[:splat].first}"
+    found = !(filename.nil? || filename.empty? || filename == '/' )
+
+    content_type :html, 'charset' => 'utf-8'
+    found ? haml(filename.to_sym, :layout => false) : "No content found for this section."
+  end
+
+  get '/' do
+    content_type :html, 'charset' => 'utf-8'
+    haml :index
+  end
 end
