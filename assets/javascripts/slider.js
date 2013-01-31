a1e3f4bdf54156b74c2ac888a6b06af2d03c0916
tcopple <J.TylerCopple@gmail.com>
2013-01-31 00:14:28 -0500

    A few changes:
      * Turned off slider when links are clicked as there's no content yet.
      * Added photos link for navigation.
      * Attempted to center navigation row.

--- assets/javascripts/slider.js
+++ assets/javascripts/slider.js
@@ -1,17 +1,17 @@
 $(document).ready( function() {
   $('navigation li').click(function() {
-    //we dont' want to take action on links that send the user outside the site
-    if($(this).hasClass('terminating')) { return }
+    // //we dont' want to take action on links that send the user outside the site
+    // if($(this).hasClass('terminating')) { return }
 
-    var clicked = $(this).attr('class');
-    var original = $("content").children().not('.hide').first();
-    var replacement = $('content').children("#"+clicked).first();
+    // var clicked = $(this).attr('class');
+    // var original = $("content").children().not('.hide').first();
+    // var replacement = $('content').children("#"+clicked).first();
 
-    original.slideUp(1500, function() {
-      original.addClass('hide');
-      replacement.slideDown(1500, function() {
-        replacement.removeClass('hide');
-      });
-    });
+    // original.slideUp(1500, function() {
+    //   original.addClass('hide');
+    //   replacement.slideDown(1500, function() {
+    //     replacement.removeClass('hide');
+    //   });
+    // });
   });
 });
